234b9efb173be4b4bb703773230c24b638134e7d
Victor Meneghini <vhmeneghini@gmail.com>
2023-01-01 19:10:10 +0000

    feat: exploring react hooks
    use useEffect to handle side effects and rerender
    on components

--- src/components/contact-page/ContactPage.js
+++ src/components/contact-page/ContactPage.js
@@ -1,12 +1,33 @@
+import { useState, useEffect } from "react";
 import { useParams, Outlet, Link } from "react-router-dom";
 
 const ContactPage = () => {
-
   const params = useParams()
+  const [user, setUser] = useState({
+    name: params.name,
+    nickname: "Meneghini"
+  })
+  
+  const fetchPokemon = () => {
+    console.log("enter here")
+    return fetch("https://pokeapi.co/api/v2/pokemon/")
+    .then((response) => response.json())
+  }
+
+  const onButtonClick = () => {
+    setUser(()=> {
+      return {...user, nickname: "changed"}
+    })
+  }
+
+  useEffect(() => {
+    fetchPokemon()
+  }, [user])
 
   return (
     <>
-      <div>Hello user: {params.name}</div>
+      <div>Hello user: { user.name}</div>
+      <button onClick={() => onButtonClick()}>Change</button>
       <Link to={"details"}>Details</Link>
       <Outlet />
     </>
